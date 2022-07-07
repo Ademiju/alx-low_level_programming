3426003628ca890f460f1df2f0337123338241a2
Ademiju Taiwo <hardaymyju@gmail.com>
2022-07-06 17:04:33 -0700

    last mandatory

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -7,29 +7,16 @@
 */
 void print_to_98(int n)
 {
-if (n <= 98)
+if (n < 98)
 {
-while (n <= 98)
-{
-if(n == 98)		
-printf("%d\n", n);
-else
-{
-printf("%d,", n);
-n = n + 1;
-}
-}
-else if(n > 98)
-{
-while (n >= 98)
-{
-if (n == 98)
-printf("%d\n", n);
-else
+for (n = n; n < 98; n++)
 printf("%d, ", n);
-n = n - 1;
+printf("%d\n", 98);
+}
+else
+{
+for (n = n; n > 98; n--)
+printf("%d, ", n);
+printf("%d\n", 98);
 }
 }
-}
-}
-
