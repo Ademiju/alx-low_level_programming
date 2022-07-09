f32d68b85a3f6bf62270273d7ead69c37de1c812
Ademiju Taiwo <hardaymyju@gmail.com>
2022-07-09 07:44:12 -0700

    101 done

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,44 +1,29 @@
 #include "main.h"
 
+int _putchar(char c);
+
 /**
-* print_number - Function that prints an integer.
-* @n: int type number
-* Description: Can only use _putchar to print.
+* print_number - print numbers
+* @n: number to be printed
+* Return: void
 */
 void print_number(int n)
 {
-long m; /* power of 10 */
-int c; /* boolean check */
-long num; /* convert int to long */
-num = n;
-/* negatives */
-if (num < 0)
-{
-num *= -1;
-_putchar('-');
-													}
-/* count up */
-m = 1;
-c = 1;
-while (c)
-{
-													if (num / (m * 10) > 0)
-													m *= 10;
-													else
-													c = 0;
-													}
+unsigned int n1 = 0;
 
-/* count down */
-while (num >= 0)
+
+if (n < 0)
 {
-													if (m == 1)
-													{
-													_putchar(num % 10 + '0');
-num = -1;
-													}
-													else
+n1 = -n;
+_putchar('-');
+}
+else
 {
-_putchar((num / m % 10) + '0');
-m /= 10;
-}													}
+n1 = n;
+}
+if (n1 / 10)
+{
+print_number(n1 / 10);
+}
+_putchar((n1 % 10) + '0');
 }
