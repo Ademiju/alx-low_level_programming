fb64c79528770a493dd0df91447ac41ea46b41ff
Ademiju Taiwo <hardaymyju@gmail.com>
2022-07-21 05:26:46 -0700

    updates

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -3,9 +3,9 @@
 void print_uint(unsigned int n);
 
 /**
- * print_number - Prints an integer
- * @n: The integer to print
- */
+* print_number - Prints an integer
+* @n: The integer to print
+*/
 void print_number(int n)
 {
 if (n < 0)
@@ -18,9 +18,9 @@ print_uint((unsigned int) n);
 }
 
 /**
- * print_uint - Prints an unsigned integer
- * @n: The unsigned int to print
- */
+* print_uint - Prints an unsigned integer
+* @n: The unsigned int to print
+*/
 void print_uint(unsigned int n)
 {
 if (n / 10 != 0)
